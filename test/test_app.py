ae4e9458124f285073b80eaaa734d5fd8db6821b
Daisy Lou M. <daisy.lou.montante@aclcbutuan.edu.ph>
2025-02-23 22:29:13 +0800

    additional test cases

--- test/test_app.py
+++ test/test_app.py
@@ -3,3 +3,17 @@ def test_addition():
 
 def test_subtraction():
     assert 5 - 3 == 2
+
+def test_multiplication():
+    assert 3 * 4 == 12
+
+def test_division():
+    assert 10 / 2 == 5
+
+def test_string_concatenation():
+    assert "Hello " + "World" == "Hello World"
+
+def test_list_append():
+    my_list = [1, 2, 3]
+    my_list.append(4)
+    assert my_list == [1, 2, 3, 4]
